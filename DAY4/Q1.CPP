1d16b8ad7bcedd77385470d0e687e831805e29c3
yyashgit <yadavyash.mail@gmail.com>
2026-06-10 00:14:32 +0530

    Add files via upload

---
+++ DAY4/Q1.CPP
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, first = 0, second = 1, next;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 0; i < n; i++) {
+        cout << first << " ";
+
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return 0;
+}

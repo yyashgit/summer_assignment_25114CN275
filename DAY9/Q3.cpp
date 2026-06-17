43abe69b2f5daff54a7acc4718c72dace8600ffa
yyashgit <yadavyash.mail@gmail.com>
2026-06-17 14:17:11 +0530

    Add files via upload

---
+++ DAY9/Q3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        char ch = 'A' + i - 1;
+
+        for (int j = 1; j <= i; j++) {
+            cout << ch;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
